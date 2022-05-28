a0fa7141b84a4e02219f7ce29d2e72d405dc6037
fateme <fs.fatemi@chmail.ir>
2022-05-28 09:58:59 +0430

    add dataBinding and viewModel for Home Fragment

--- app/src/main/java/com/example/mystor/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mystor/ui/home/HomeFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.example.mystor.R
+import com.example.mystor.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    val vModel: HomeViewModel by viewModels()
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_home, container, false
+        )
+        binding.vModel = vModel
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        return binding.root
     }
 
 }
