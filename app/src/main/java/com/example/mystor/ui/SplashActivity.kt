4d85760512e66953e22c40f7016cb090dd483208
fateme <fs.fatemi@chmail.ir>
2022-05-28 11:27:38 +0430

    add splash screen

--- app/src/main/java/com/example/mystor/ui/SplashActivity.kt
+++ app/src/main/java/com/example/mystor/ui/SplashActivity.kt
@@ -2,11 +2,28 @@ package com.example.mystor.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mystor.R
+import com.example.mystor.databinding.ActivityMainBinding
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        supportActionBar?.hide()
+        binding.tvAppName.alpha = 0f
+        binding.tvAppName.animate().setDuration(3000).alpha(1f).withEndAction {
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
+        binding.iconSplash.alpha= 0f
+        binding.iconSplash.animate().setDuration(3000).alpha(1f).withEndAction {
+            binding.fragmentContainerView.visibility = View.VISIBLE
+            binding.layout.visibility = View.GONE
+            supportActionBar?.show()
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
     }
 }
